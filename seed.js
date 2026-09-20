6b9c0bf08f8e2f00a187a5b30e1b62d58b91ce32
Mauricio Machado <mau87machado@gmail.com>
2026-09-20 12:10:41 -0300

    Usa arte especial de Solaris

--- seed.js
+++ seed.js
@@ -1440,7 +1440,7 @@ window.BIBLIOTECA_SEED = [
     "priority": "",
     "targetPrice": 0,
     "limitPrice": 0,
-    "coverUrl": "https://image.tmdb.org/t/p/w500/pgqj7QoBPWFLLKtLEpPmFYFRMgB.jpg",
+    "coverUrl": "https://mmachadocode-labs.github.io/biblioteca-bluray/covers/solaris-edicao-especial.jpg",
     "tags": [
       "Blu-ray",
       "Edição especial",
